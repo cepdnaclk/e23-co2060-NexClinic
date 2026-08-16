b916163794126d94ef9ab237d134dbd0da870d7c
tdineth <e23076@eng.pdn.ac.lk>
2026-08-16 23:11:01 +0530

    Update

--- backend/hospital/urls.py
+++ backend/hospital/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     ActivityLogListView,
     DoctorAppointmentAnalyticsView,
+    HospitalAppointmentListView,
     ReportsView,
     AvailableDoctorsView,
     ActiveHospitalListView,
@@ -13,13 +14,30 @@ from .views import (
 )
 
 urlpatterns = [
-    path('active/', ActiveHospitalListView.as_view(), name='hospital-active-list'),
-    path('profile/', HospitalAdminProfileView.as_view(), name='hospital-admin-profile'),
-    path('activity-logs/', ActivityLogListView.as_view(), name='hospital-activity-logs'),
-    path('reports/', ReportsView.as_view(), name='hospital-reports'),
-    path('appointment-analytics/', DoctorAppointmentAnalyticsView.as_view(), name='hospital-appointment-analytics'),
-    path('available-doctors/', AvailableDoctorsView.as_view(), name='available-doctors'),
-    path('manage-doctor/', ManageHospitalDoctorView.as_view(), name='manage-doctor'),
-    path('create-doctor/', CreateHospitalDoctorView.as_view(), name='create-doctor'),
-    path('doctor-fees/<int:doctor_id>/', ManageDoctorFeesView.as_view(), name='manage-doctor-fees'),
+    path("active/", ActiveHospitalListView.as_view(), name="hospital-active-list"),
+    path("profile/", HospitalAdminProfileView.as_view(), name="hospital-admin-profile"),
+    path(
+        "activity-logs/", ActivityLogListView.as_view(), name="hospital-activity-logs"
+    ),
+    path("reports/", ReportsView.as_view(), name="hospital-reports"),
+    path(
+        "appointment-analytics/",
+        DoctorAppointmentAnalyticsView.as_view(),
+        name="hospital-appointment-analytics",
+    ),
+    path(
+        "appointments/",
+        HospitalAppointmentListView.as_view(),
+        name="hospital-appointments",
+    ),
+    path(
+        "available-doctors/", AvailableDoctorsView.as_view(), name="available-doctors"
+    ),
+    path("manage-doctor/", ManageHospitalDoctorView.as_view(), name="manage-doctor"),
+    path("create-doctor/", CreateHospitalDoctorView.as_view(), name="create-doctor"),
+    path(
+        "doctor-fees/<int:doctor_id>/",
+        ManageDoctorFeesView.as_view(),
+        name="manage-doctor-fees",
+    ),
 ]
